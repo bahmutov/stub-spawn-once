6f272d6aef8a1d19e559792d9493d07d4218ea09
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-09 14:42:30 -0400

    initial

---
+++ src/stub-spawn-once-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const stubSpawnOnce = require('.')
+
+describe('stub-spawn-once', () => {
+  it('write this test', () => {
+    console.assert(stubSpawnOnce, 'should export something')
+  })
+})
